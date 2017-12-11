6cc39a44dcc226122425ee26bf1917837934d717
liuzuobin <zuobin@shanshu.ai>
2017-12-12 01:35:53 +0800

    音频提交

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-date_default_timezone_set("Asia/Shanghai");
+
 class Main extends MY_Controller {
 
 	public function __construct() {
@@ -29,20 +29,7 @@ class Main extends MY_Controller {
 
 	//register
 	public function register()
-	{	
-		if($_POST){
-			$postinfo= $this->Common->html_filter_array($_POST);
-			$time=time();
-			$add_data=array('username' => $postinfo['phone'],'nickname' => $postinfo['username'],'password' => $postinfo['password'],'ctime' =>$time);
-			$rep=$this->Common->add($this->user_table,$add_data);
-			if($rep>0){
-				redirect('main/index');
-				// $data['postinfo']=$postinfo;
-				// $this->load->view('header');
-				// $this->load->view('main/testdemo',$data);
-				// $this->load->view('footer');
-			}
-		}
+	{
 		$this->load->view('header');
 		$this->load->view('user/register');
 		$this->load->view('footer');
@@ -77,6 +64,13 @@ class Main extends MY_Controller {
 		$this->load->view('header');
 		$this->load->view('audio/audio');
 		$this->load->view('footer');	
+	}
+
+	//audioinfo
+	public function audioInfo()
+	{
+		$this->load->view('header');
+		$this->load->view('audio/audioinfo');
 	}
 
 	//视频链接
@@ -160,39 +154,34 @@ class Main extends MY_Controller {
 		if($_POST){
 			$postinfo= $this->Common->html_filter_array($_POST);
 			$data['postinfo']=$postinfo;
-			redirect(base_url('main/index'));
+
 			// $where=array('id' => '10');
 			// $sql_data=$this->Common->get_all($this->user_table,$where);
-			// $select_field='id,username,nickname';
+			$select_field='id,username,nickname';
 			// $sql_data=$this->Common->get_one($this->user_table,$where,$select_field);
 			// $where=array();
-			// $start=0;	
-			// $like=array('nickname' => '张');
-			// $orderby='id';
+			$start=0;	
+			$like=array('nickname' => '张');
+			$orderby='id';
 			//$sql_data=$this->Common->get_limit_order( $this->user_table, $where, $start,$this->per_page,'ctime','DESC',$like);
 			
 			// $where=array('id' => '100','offPrice'=>'200');
 			// $sql_data=$this->Common->delete($this->user_table,$where);
 			// $where=array('id' => '100');
-			// $up_data=array('offPrice' => '300','weixin'=>'gongkun');
+			$up_data=array('offPrice' => '300','weixin'=>'gongkun');
 			// $sql_data=$this->Common->update($this->user_table,$where,$up_data);
 
-			// $insert_data=array('username' => '18910111135','password'=>'123456','nickname'=>'fuck123','user_type'=>'2','weixin'=>'gongkun123','if_check'=>'0');
+			$insert_data=array('username' => '18910111135','password'=>'123456','nickname'=>'fuck123','user_type'=>'2','weixin'=>'gongkun123','if_check'=>'0');
 			// $sql_data = $this->Common->add($this->user_table,$insert_data);
-			// $where=array();
+			$where=array();
 			// $sql_data = $this->Common->get_count($this->user_table,$where,$like);
 
 			// $sql_data = $this->Common->save($this->user_table,$insert_data);
 
 
 
-			// $data['sql_data'] = $sql_data;
+			$data['sql_data'] = $sql_data;
 		}
-		// $where=array();
-		// $select_field='id,username,nickname';
-		// $sql_data=$this->Common->get_one($this->user_table,$where,$select_field);
-		// $data['sql_data'] = $sql_data;
-		// $data['length'] = count($sql_data);
 
 		$data['data']=$data;
 		$this->load->view('header');
@@ -204,37 +193,8 @@ class Main extends MY_Controller {
 	//测试跳转
 	public function message()
 	{
-		$tmp_data['title']='message';
-		// send message
-		$authnum='';
-        $ychar = "0,1,2,3,4,6,7,8,9";  
-		$list = explode(",",$ychar);  
-		for($i=0; $i<4; $i++)
-		{  
-			$randnum = mt_rand( 0,8 );							  
-			$authnum .= $list[$randnum];  
-		}
-		$this->SmsDemo->initParm($this->smsKeyId,$this->smsKeySecret);
-		//调用发送函数
-		$response = $this->SmsDemo->sendSms(
-			$this->smsSignName,   // 短信签名
-			$this->smsTemplate,   // 短信模板编号
-			'18916294857',   // 短信接收者
-			Array(     // 短信模板中字段的值
-			        "code"=> $authnum,
-			        "product"=>"xinshiwang"
-			),
-			"123"
-		);
-		if($response->Code == "OK"){
-			$tmp_data['ok']='Yes';
-		}
-
-
-		$data['data']=$tmp_data;
-		$this->load->view('header');
-		$this->load->view('main/message',$data);
-		$this->load->view('footer');
+		$data['title']='message';
+		$this->load->view('welcome_message');
 	}
 
 	// //视频链接
@@ -252,6 +212,10 @@ class Main extends MY_Controller {
 		$this->load->view('header');
 		$this->load->view("main/chronic_disease");
 		$this->load->view('footer');
+	}
+
+	public function testPPT(){
+		$this->load->view('main/testPPT');
 	}
 }
 
