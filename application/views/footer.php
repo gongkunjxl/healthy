6cbe0855e52b4fd0d60178d818857b061b256201
gongkunjxl@163.com <gongkunjxl@163.com>
2018-03-23 11:30:15 +0800

    picture

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,26 +3,18 @@
 	<div class="layui-container">
 		<div class="footer-content">
 			<ul>
-				<li>ICP证粤B2-2000000信用中国</li>
-				<li>增值电信业务许可证</li>
-				<li>国家网络出版集团</li>
-				<li>互联网药品信息服务资格证书</li>
+				<li>学术支持</li>
+				<li>国家心血管病中心防治资讯部</li>
+				<li>中华预防医学会健康传播分会</li>
 			</ul>
 		</div>
 		<div class="footer-content">
 			<ul>
-				<li>国际互联网备案中心</li>
-				<li>中国互联网举报中心</li>
-				<li>中国互联网信息平台</li>
-				<li>互联网药品信息服务资格证书</li>
-			</ul>
-		</div>
-		<div class="footer-content">
-			<ul>
-				<li>南京医学院</li>
-				<li>电话: 1772817181</li>
-				<li>地址: 北京市海淀区双清路1号</li>
-				<li>技术支持: 中国科工集团</li>
+				<li>指导单位</li>
+				<li>国家卫生和计划生育委员会疾病预防控制局</li>
+				<li>国家卫生和计划生育委员会宣传司</li>
+				<li> 中国健康教育中心</li>
+				<li> 中国记协办公室</li>
 			</ul>
 		</div>
 	</div>
