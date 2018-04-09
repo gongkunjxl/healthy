3e7bee690463218098eeb7411e589a7308df633e
root <gongkunjxl@163.com>
2018-04-09 10:15:14 +0800

    new

--- application/views/header2.php
+++ application/views/header2.php
@@ -50,18 +50,25 @@ if(isset($_SESSION['userid']) && $_SESSION['userid']>0){
 </head>
 <body >
 <div  class="div-layout">
+<div class="layui-container" style="display:inline-block;height: 105px;">
+    
+    <ul style="display: inline;margin-left:auto;margin-right:auto;">
+       <img src="/static/images/激励计划镂空.png" style="float: left;width: 100px;height: 100px;margin-top: 1px;">
+       <p style="font-size:x-large;font-family: serif;font-weight: bold;color: #00B2EE;margin-top: 20px;margin-bottom: 15px;">中国健康知识传播激励计划</p>
+       <p style="font-size: xx-large;font-family: serif;font-weight: bolder;color: #009ACD;">健 康 科 普 资 源 库</p>
+    </ul>
+    
+  </div>
 <div class="nav">
   <div class="layui-container">
     <div class="logo">
-        <img src="/static/images/head_logo.png">
     </div>
     <!-- <img class="logo" src="/static/images/head_logo.png"   alt="logo"> -->
-   <ul class="layui-nav">
-       <h1>健康科普资源库</h1>
-        <li class="layui-nav-item">
+   <ul class="layui-nav" style="margin-top: 10px;" > 
+        <li class="layui-nav-item" style="line-height: 30px;">
           <a style="text-decoration: none; color: white; font-size: 16px;" href="/main/index">首页</a>
         </li>
-        <li class="layui-nav-item" style="padding-right:10px;">
+        <li class="layui-nav-item" style="padding-right:10px;line-height: 30px;">
           <a style="text-decoration: none; color: white; font-size: 16px;">慢性疾病</a>
           <dl class="layui-nav-child">
             <dd style="line-height: 36px;"><a style="text-decoration: none;" href="/main/searchIndex/1/1">高血脂</a></dd>
@@ -69,7 +76,7 @@ if(isset($_SESSION['userid']) && $_SESSION['userid']>0){
             <dd style="line-height: 36px;"><a style="text-decoration: none;" href="/main/searchIndex/1/3">高血压</a></dd>
           </dl>
         </li>
-        <li class="layui-nav-item" style="margin-left: 1%;">
+        <li class="layui-nav-item" style="margin-left: 1%;line-height: 30px;">
           <a style="text-decoration: none; color: white; padding-right: 20px; font-size: 16px;">健康生活方式</a>
           <dl class="layui-nav-child">
             <dd style="line-height: 36px;"><a style="text-decoration: none;" href="/main/searchIndex/2/4">情绪管理</a></dd>
@@ -81,7 +88,7 @@ if(isset($_SESSION['userid']) && $_SESSION['userid']>0){
     <div class="right-nav">
       <?php if($userid>0):?>
         <ul class="layui-nav" style="margin-top: 0;width: 100%;">
-           <li class="layui-nav-item" style="max-width: 180px;">
+           <li class="layui-nav-item" style="max-width: 180px;line-height: 30px;">
               <a style="text-decoration: none; margin-right: 20px; max-width: 150px; overflow: hidden; white-space:nowrap;"><img src="/static/images/header_1.jpg" class="layui-nav-img"><?php echo $nickname; ?></a>
               <dl class="layui-nav-child">
                 <?php if($type==2): ?>
